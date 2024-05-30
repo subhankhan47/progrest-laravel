da8bdd971d287c183474c77c0dad24d37a9004d5
Subhan Khan <subhankhan71234@gmail.com>
2024-05-29 20:35:47 -0700

    # Modified Design Implemented =>Cosmetic Changes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,7 +13,7 @@
     <!-- Slick Slider CSS -->
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"/>
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"/>
-    <title>Progrest</title>
+    <title>StrongHoldListing</title>
 </head>
 <body>
 
